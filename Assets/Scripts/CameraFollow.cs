c7836915b67cc857af9af614de29a308405d91b0
todormilchev160 <bavnotora@gmail.com>
2026-06-16 11:21:17 +0300

    Camera follow with lerp

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]private Transform player;
+    [SerializeField] private float  followSpeed=0.3f;
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(player.position.x, player.position.y+1,transform.position.z);
+        transform.position=Vector3.Lerp(transform.position,targetPosition,followSpeed*Time.deltaTime);   
+    }
+}
