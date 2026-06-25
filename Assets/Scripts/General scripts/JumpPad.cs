5e204f84c5b525cdd0b9315513e6c163db1b32eb
todormilchev160 <bavnotora@gmail.com>
2026-06-25 18:25:37 +0300

    Player turning

--- Assets/Scripts/General scripts/JumpPad.cs
+++ Assets/Scripts/General scripts/JumpPad.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     [SerializeField]private int launchforce=3;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnCollisionEnter(Collision collision)
     {
       Rigidbody rb =collision.gameObject.GetComponent<Rigidbody>();
-              rb.linearVelocity = new Vector3(
+            rb.linearVelocity = new Vector3(
             rb.linearVelocity.x,
             launchforce,
             rb.linearVelocity.z
